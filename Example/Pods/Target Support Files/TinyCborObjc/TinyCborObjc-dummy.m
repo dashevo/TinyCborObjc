4b53d906ceae75bc37d5c422a00cdc9f867a67b1
Hamilton Chapman <hamchapman@gmail.com>
2020-03-04 17:05:33 +0000

    Restructure project to make it easier to develop with

--- Example/Pods/Target Support Files/TinyCborObjc/TinyCborObjc-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TinyCborObjc : NSObject
-@end
-@implementation PodsDummy_TinyCborObjc
-@end
--- Example/Pods/Target Support Files/tinycbor/tinycbor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_tinycbor : NSObject
-@end
-@implementation PodsDummy_tinycbor
-@end
