42791f37259e41aa1baf91b1f1e3630e15eb11b8
Connor Power <connor@connorpower.com>
2020-04-22 18:43:40 +1200

    Shift ObjCCBOR class encoder from monorepo into this project

---
+++ ObjCCBOR/ObjCCBOR.h
@@ -0,0 +1,20 @@
+//
+//  ObjCCBOR.h
+//  ObjCCBOR
+//
+//  Created by Hamilton Chapman on 20/02/2020.
+//  Copyright © 2020 Ditto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjCCBOR : NSObject
+
++ (nullable id)decodeCbor:(NSData *)data error:(NSError * _Nullable __autoreleasing *)error;
++ (NSData *)encodeCbor:(NSObject *)object error:(NSError * _Nullable __autoreleasing *)error;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjCCBOR/ObjCCBOR.m
@@ -0,0 +1,33 @@
+//
+//  ObjCCBOR.m
+//  ObjCCBOR
+//
+//  Created by Hamilton Chapman on 20/02/2020.
+//  Copyright © 2020 Ditto. All rights reserved.
+//
+
+#import "ObjCCBOR.h"
+#import "NSData+ObjCCBOR.h"
+#import "NSObject+ObjCCBOR.h"
+
+@implementation ObjCCBOR
+
++ (nullable id)decodeCbor:(NSData *)data error:(NSError * _Nullable __autoreleasing *)error {
+    NSError *err = nil;
+    id decoded = [data ds_decodeCborError:&err];
+    if (error && err != nil) {
+        *error = err;
+    }
+    return decoded;
+}
+
++ (NSData *)encodeCbor:(NSObject *)object error:(NSError * _Nullable __autoreleasing *)error {
+    NSError *err = nil;
+    NSData *encoded = [object ds_cborEncodedObjectError:&err];
+    if (error && err != nil) {
+        *error = err;
+    }
+    return encoded;
+}
+
+@end
